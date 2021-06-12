43ba212a1df30ef43927dec9acccf60a147b67ad
z9877969 <z9877969@ukr.net>
2021-06-12 14:14:40 +0300

    l-5

--- src/components/MainInfo/MainInfo.jsx
+++ src/components/MainInfo/MainInfo.jsx
@@ -1,16 +1,29 @@
 import Section from "../_share/Section/Section";
+import icons from "../../assets/icons/sprite.svg";
+import css from "./MainInfo.module.css";
+import IconAdd from "../_icons/IconAdd";
 
 const MainInfo = ({ title, btnTitle, options, transType, handleClick }) => {
+  console.log(icons);
   return (
     <Section title={title}>
-      <button type="button" onClick={() => handleClick(transType)}>{btnTitle}</button>
+      <button
+        className={css.btn}
+        type="button"
+        onClick={() => handleClick(transType)}
+      >
+        {/* <svg className={css.icon}>
+          <use href={icons + "#icon-add-outline"}></use>
+        </svg> */}
+        <IconAdd className={css.icon} />
+      </button>
       <span>RUB</span>
       <ul>
         {options.map(({ id, descr }) => (
           <li key={id}>
             <span>{descr}</span>
             <span>0.00</span>
-          </li> 
+          </li>
         ))}
       </ul>
     </Section>
